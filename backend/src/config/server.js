6e5ebdd4674e3a359301817a56a6a5b26a360ace
Leonardo Rebouças <leoabcblm@gmail.com>
2022-09-04 22:16:33 -0300

    Configurando banco de dados

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,7 +4,10 @@ const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json())
 
 server.listen(port, function() {
     console.log(`BACKEND is running on port: ${port}.`)
 })
+
+module.exports = server
